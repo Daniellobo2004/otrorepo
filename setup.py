985ee9cb638f2283990fd7333bed44f590eace12
lobodaniel279@gmail.com <lobodaniel279@gmail.com>
2025-05-19 00:09:12 -0400

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="otrorepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Daniel",                         # Tu nombre
+    author_email="lobodaniel279@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Daniellobo2004/otrorepo",     # URL del proyecto
+)
